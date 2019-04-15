ef82051213ade4b03a14270e6f7146d7a645dbf2
Charlotte Neff <charlotte.neff@gmail.com>
2019-04-15 22:26:55 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,12 +1,14 @@
-class CreateCostumes < ActiveRecord::Migration 
+class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
-      t.string :name 
+      t.string :name
       t.integer :price
+      t.string :image_url
       t.integer :size
+      t.timestamp :created_at
+      t.timestamp :updated_at
     end
   end
-  
 end
 
 
