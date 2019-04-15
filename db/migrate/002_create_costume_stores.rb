4c68645b75d47a47452907953c5657a96c04d51b
Charlotte Neff <charlotte.neff@gmail.com>
2019-04-15 22:31:19 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,11 @@
 class CreateCostumeStores
   def change
-    create_table
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.
   
   
   
