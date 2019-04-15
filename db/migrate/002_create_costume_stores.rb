440391a9421de2f90a3972d8eacbf56e83898858
Charlotte Neff <charlotte.neff@gmail.com>
2019-04-15 22:31:54 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,8 @@ class CreateCostumeStores
       t.string :name 
       t.string :location
       t.integer :costume_inventory
-      t.integer :number_of_employees
+      t.integer :num_of_employees
+      t.boolean :still_in_business
       t.
   
   
